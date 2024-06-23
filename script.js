8e754a51e952dd129dd337aa4aa9df83a42aad1e
roycechoco <roystoncheng154@gmail.com>
2024-06-24 03:51:08 +0800

    Saved files to use on Macbook

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ for (let i=0; i<=15;i++)
        
         const rowDiv = document.createElement('div');
 
-        rowDiv.className = 'row-div';
+        rowDiv.className = 'rowdiv';
 
         
 
     
       
         const container = document.getElementById('container');
-
+        rowDiv.innerText = 'Test';
         container.appendChild(rowDiv);
 
     };
