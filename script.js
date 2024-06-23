43a44d41e6f66789a3143bde44de4c439e5b7fbc
roycechoco <roystoncheng154@gmail.com>
2024-06-24 00:34:40 +0800

    Created 16 row divs with DOM method and put it inside a container div

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const body=document.body;
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+for (let i=0; i<=15;i++)
+    {   
+       
+        const rowDiv = document.createElement('div');
+
+        rowDiv.className = 'row-div';
+
+        
+
+    
+      
+        const container = document.getElementById('container');
+
+        container.appendChild(rowDiv);
+
+    };
+
+  
+});
+
+
