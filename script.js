7d8482f5bf25917e2329690cf1776018e77a0d2f
roycechoco <roystoncheng154@gmail.com>
2024-06-27 13:59:31 +0800

    updating the work to transfer to my PC

--- script.js
+++ script.js
@@ -4,34 +4,39 @@ const body=document.body;
 
 document.addEventListener('DOMContentLoaded', function() {
 
-
+    let divs =[];
 
     function divCreate (noOfDivs) 
     { 
-        
+        let divs =[];
            
-                for (let i=1; i<=(noOfDivs*noOfDivs); i++)
+                  for (let i=1; i<=(noOfDivs*noOfDivs); i++)
                     {
                     const divStyle = document.createElement('div');
                     divStyle.className = 'divstyle';
-                    
+                    divStyle.textContent = `${i}`;
                     const container = document.querySelector('.container');
                     container.append(divStyle);
                     console.log("i");
                      }
 
                 
-           
-
-            
-
-
-
+        
 
     }
-
     divCreate(16);
 
+
+   
+
+
+                            
+    
+    
+
+    
+
+    
 
 
   
